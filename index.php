d6eb97ab6099d775628550d6b8fe705edd72467c
efik1 <efimov.vladislav99@mail.ru>
2023-10-04 22:55:00 +0500

    Init

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+$f_json = 'D:\Programms\Projects\test.json';
+$json = file_get_contents("$f_json");
+$arr = json_decode($json, true);
+$arr2 = array();
+foreach ($arr['Остров'] as $key => $value)
+{
+    if($value['rb_fact'] != NULL)
+    $arr2[$key]['rb_fact'] = $value['rb_fact'];
+}
+file_put_contents("encode.json", json_encode($arr2));
