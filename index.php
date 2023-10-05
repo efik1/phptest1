aac44d13c269d5eb04dd13d573d4eae72f626e92
efik1 <efimov.vladislav99@mail.ru>
2023-10-05 19:10:27 +0500

    Changed

--- index.php
+++ index.php
@@ -8,4 +8,4 @@ foreach ($arr['Остров'] as $key => $value)
     if($value['rb_fact'] != NULL)
     $arr2[$key]['rb_fact'] = $value['rb_fact'];
 }
-file_put_contents("encode.json", json_encode($arr2));
+file_put_contents("encode.json", json_encode($arr2,JSON_PRETTY_PRINT));
